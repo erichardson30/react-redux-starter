7e2f1d460af1af8a58f29be30a270ec78f9d5a1e
Eric <eric.richardson30@gmail.com>
2017-03-08 10:48:32 -0500

    login link toggles when the user is logged in/out

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
 
 import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const isLoggedIn = this.props.isLoggedIn;
     return (
       <div className="App">
         <div className="App-header">
@@ -14,7 +16,7 @@ class App extends Component {
           <div className="app-nav">
             <nav>
               <Link to='about'>About</Link>
-              <Link to='login'>Login</Link>
+              <Link to='login'>{( isLoggedIn ? 'Logout' : 'Login' )}</Link>
               <Link to='protected'>Protected</Link>
             </nav>
           </div>
@@ -24,5 +26,15 @@ class App extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn:  (state.login.loggedIn ? state.login.loggedIn : false) 
+  }
+}
+
+App = connect (
+  mapStateToProps
+)(App)
 
 export default App;
